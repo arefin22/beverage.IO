c2d71699ab352db69c95dc6fc3a9c3895425325a
arefin22 <shamsularefin500@gmail.com>
2023-10-21 20:39:32 +0600

    Brand Condition Added

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -5,25 +5,40 @@ import StarRating from "./StarRating";
 
 const Item = ({ item }) => {
 
+    console.log(item);
+
     return (
         <div className="card glass">
-            <figure className="max-h-72"><img src={item.photo} alt={item.name} /></figure>
-            <div className="card-body">
-                <div>
-                    <div className="badge badge-outline">{item.brand}</div>
-                    <h2 className="card-title">{item.name}</h2>
-                    <p>{item.type}</p>
-                </div>
-                <div>
-                    <StarRating rating={item.rating} />
-                    {/* <p><span className="font-medium">Rating</span> : {item.rating}</p> */}
-                    <p><span className="font-medium">Price</span> : ${item.price}</p>
-                </div>
-                <div className="card-actions justify-center">
-                    <Link to={`/details/${item._id}`} className="btn btn-outline btn-primary">Details</Link>
-                    <Link to={`/editItem/${item._id}`} className="btn btn-outline btn-secondary">Update</Link>
-                </div>
-            </div>
+            {
+                item ? <>
+                    <figure className="max-h-72"><img src={item.photo} alt={item.name} /></figure>
+                    <div className="card-body">
+                        <div>
+                            <div className="badge badge-outline">{item.brand}</div>
+                            <h2 className="card-title">{item.name}</h2>
+                            <p>{item.type}</p>
+                        </div>
+                        <div>
+                            <StarRating rating={item.rating} />
+                            {/* <p><span className="font-medium">Rating</span> : {item.rating}</p> */}
+                            <p><span className="font-medium">Price</span> : ${item.price}</p>
+                        </div>
+                        <div className="card-actions justify-center">
+                            <Link to={`/details/${item._id}`} className="btn btn-outline btn-primary">Details</Link>
+                            <Link to={`/editItem/${item._id}`} className="btn btn-outline btn-secondary">Update</Link>
+                        </div>
+                    </div>
+                </>
+
+                   :
+                    <div className="mx-auto text-center">
+                        <Link className="btn btn-outline btn-error" to={'/addItem'}> Add Some Item To Show </Link>
+                        <br />
+                        Or
+                        <br />
+                        <Link to={'/'} className="btn btn-outline btn-primary">Back To Home</Link>
+                    </div>
+            }
         </div>
     );
 };
--- src/components/ItemsBanner.jsx
+++ src/components/ItemsBanner.jsx
@@ -1,24 +1,31 @@
 
 const ItemsBanner = () => {
+
+
+    //     https://i.ibb.co/6R5BjbD/slide-3.jpg
+    // https://i.ibb.co/3B71LG1/pepsi.webp
+    // https://i.ibb.co/ynXdjzS/slide-1.jpg
+
+
     return (
         <div className=" pb-40">
             <div className="carousel max-h-96  w-full ">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <figure className="flex items-center"><img src="https://i.ibb.co/fQbfdKb/Coca-Cola.jpg" className="w-full" /></figure>
+                    <figure className="flex items-center w-full"><img src="https://i.ibb.co/6R5BjbD/slide-3.jpg" className="max-h-screen w-full" /></figure>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <figure className="flex items-center"><img src="https://i.ibb.co/NxX5WgJ/Starbucks.jpg" className="w-full" /></figure>
+                    <figure className="flex items-center w-full"><img src="https://i.ibb.co/3B71LG1/pepsi.webp" className="max-h-screen w-full" /></figure>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <figure className="flex items-center"><img src="https://i.ibb.co/TMSr7Ph/cafe-Img.jpg" className="w-full" /></figure>
+                    <figure className="flex items-center w-full"><img src="https://i.ibb.co/ynXdjzS/slide-1.jpg" className="max-h-screen w-full" /></figure>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide1" className="btn btn-circle">❯</a>
